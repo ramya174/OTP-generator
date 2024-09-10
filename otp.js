7316a99161d242b47cd3ebb3cdbabcb17113e90a
Ramya <pramya174@gmail.com>
2024-09-10 06:50:31 -0700

    Developed random OTP generator

---
+++ otp.js
@@ -0,0 +1,66 @@
+//Way1: OTP with digits that range from 4 to 8
+
+let input1 = document.getElementById("userInput");
+let getButton = document.getElementById("get");
+let randomValue = document.getElementById("randomNum");
+
+getButton.addEventListener('click', () => {
+    let data = input1.value;
+
+    if(!data){
+        alert("Please enter some value");
+
+    }else if(data == 4){
+        const num = Math.floor(Math.random()*(10000));
+        
+        randomValue.textContent = num;
+        randomValue.style.color = "green";
+    }else if(data == 5){
+        const num = Math.floor(Math.random()*100000);
+        
+        randomValue.textContent = num;
+        randomValue.style.color = "green"
+    }else if(data == 6){
+        const num = Math.floor(Math.random()*1000000);
+        
+        randomValue.textContent = num;
+        randomValue.style.color = "green"
+    }else if(data == 7){
+        const num = Math.floor(Math.random()*10000000);
+        
+        randomValue.textContent = num;
+        randomValue.style.color = "green"
+    }else if(data == 8){
+        const num = Math.floor(Math.random()*100000000);
+        
+        randomValue.textContent = num;
+        randomValue.style.color = "green"
+    }else{
+        randomValue.textContent = "Enter valid number!!!"
+        randomValue.style.color = "red"
+    }
+    
+})
+
+
+
+//Way2: OTP with given input digit
+
+// let input1 = document.getElementById("userInput");
+// let getButton = document.getElementById("get");
+// let randomValue = document.getElementById("randomNum");
+
+// getButton.addEventListener('click', () => {
+//     let data = input1.value;
+
+//     if(!data){
+//         alert("Please enter some value");
+
+//     }else{
+//         const num = Math.floor(Math.random()*(10**data));
+        
+//         randomValue.textContent = num;
+//         randomValue.style.color = "green";
+//     }
+    
+// })
